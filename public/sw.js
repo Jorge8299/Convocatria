c9195533478cbe05f9ba3f9e28b8eba519a0e167
Jorge8299 <jorgeholawifi@gmail.com>
2026-08-22 23:40:16 +0200

    Hacer visible el control del banquillo

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'convo-shell-v2';
+const CACHE = 'convo-shell-v3';
 const SHELL = ['/', '/manifest.webmanifest', '/escudo-ud-oliva.jpg'];
 self.addEventListener('install', (event) => { event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener('activate', (event) => { event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))); self.clients.claim(); });
